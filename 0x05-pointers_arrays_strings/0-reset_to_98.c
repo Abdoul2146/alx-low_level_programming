03e461566acd78a8012b9cbeeaef3f9d763b9ca7
Abdulhakim Abdurrazak Sulaiman <abdulhakim.a.sulaiman50@gmail.com>
2022-10-24 06:18:13 -0700

    Keygen

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * reset_to_98
- * @n: declaration of n and paramters for the function reset_to_98
- * Return: Always 0.
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: n
  */
 
 void reset_to_98(int *n)
